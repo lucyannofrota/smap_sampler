f8a3abb2d0448c5c6a767b191dc8ff92471b6191
Lucyanno Frota <lucyannofrota@gmail.com>
2023-03-14 18:13:11 +0000

    Callback Groups and Executors setup

--- src/smap_sampler_node.cpp
+++ src/smap_sampler_node.cpp
@@ -7,7 +7,9 @@
 #include "sensor_msgs/msg/image.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
 
-// ZED2 Camera
+#include <semaphore.h>
+
+// ZED2 Camera info
 // Image
 //    /zed2/zed_node/rgb/image_rect_color
 //    sensor_msgs/msg/Image
@@ -15,9 +17,7 @@
 //    /zed2/zed_node/point_cloud/cloud_registered
 //    sensor_msgs/msg/PointCloud2
 
-// Node
 
-// TODO: Configure callback groups
 #define MAP_FRAME std::string("map")
 #define BASE_LINK_FRAME std::string("base_link")
 #define CAMERA_FRAME std::string("zed2_left_camera_frame")
@@ -27,33 +27,31 @@ using std::placeholders::_1;
 namespace smap
 {
 
-class smap_sampler : public rclcpp::Node
+class smap_sampler_node : public rclcpp::Node
 {
 private:
-  //** Variables **//
+  // Variables
+  sem_t sem_image, sem_pcl;
   sensor_msgs::msg::Image::SharedPtr last_image_msg;
   sensor_msgs::msg::PointCloud2::SharedPtr last_pcl2_msg;
+
+  // Callback Group
+  rclcpp::CallbackGroup::SharedPtr reentrant_cb_group{nullptr};
+  rclcpp::SubscriptionOptions sub_options;
+  //rclcpp::SubscriptionOptionsWithAllocator<rclcpp::CallbackGroupType> sub_options;
 
   // TF
   std::unique_ptr<tf2_ros::Buffer> tf_buffer;
   std::shared_ptr<tf2_ros::TransformListener> tf_listener{nullptr};
 
   // Publisher
-  rclcpp::Publisher<smap_interfaces::msg::SmapData>::SharedPtr SmapData_pub = this->create_publisher<smap_interfaces::msg::SmapData>("/smap/sampler/data", 10);
-  rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_pub = this->create_publisher<geometry_msgs::msg::PoseStamped>("/smap/sampler/pose", 10);
-  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pcl_pub = this->create_publisher<sensor_msgs::msg::PointCloud2>("/smap/sampler/pcl", 10);
+  rclcpp::Publisher<smap_interfaces::msg::SmapData>::SharedPtr SmapData_pub;
+  rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_pub;
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pcl_pub;
 
   // Subscriptions
-  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub = this->create_subscription<sensor_msgs::msg::Image>(
-    "/zed2/zed_node/rgb/image_rect_color",
-    10,
-    std::bind(&smap::smap_sampler::image_callback, this, _1)
-  );
-  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pcl2_sub = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-    "/zed2/zed_node/point_cloud/cloud_registered",
-    10,
-    std::bind(&smap::smap_sampler::pcl2_callback, this, _1)
-  );
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub;
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pcl2_sub;
 
   // Timer
   rclcpp::TimerBase::SharedPtr pose_timer{nullptr};
@@ -61,10 +59,18 @@ private:
 
 public:
   // Constructor/Destructor
-  smap_sampler()
-  : Node("smap_sampler")
+  smap_sampler_node()
+  : Node("smap_sampler_node")
   {
-    RCLCPP_INFO(this->get_logger(), "Initializing smap_sampler");
+    RCLCPP_INFO(this->get_logger(), "Initializing smap_sampler_node");
+
+    // Callback group setup
+    this->reentrant_cb_group = this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+    this->sub_options.callback_group = this->reentrant_cb_group;
+
+    // Initializing Semaphores
+    sem_init(&this->sem_image,0,1);
+    sem_init(&this->sem_pcl,0,1);
 
     // tf buffer
     tf_buffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
@@ -74,7 +80,7 @@ public:
     this->smap_data_timer = this->create_wall_timer(
       std::chrono::milliseconds(250), // Change Frequency
       std::bind(
-        &smap_sampler::data_sampler,
+        &smap_sampler_node::data_sampler,
         this
       )
     );
@@ -82,13 +88,36 @@ public:
     this->pose_timer = this->create_wall_timer(
       std::chrono::milliseconds(50), // Change Frequency
       std::bind(
-        &smap_sampler::pose_sampler,
+        &smap_sampler_node::pose_sampler,
         this
       )
     );
+
+    // Publisher
+    SmapData_pub = this->create_publisher<smap_interfaces::msg::SmapData>("/smap/sampler/data", 10);
+    pose_pub = this->create_publisher<geometry_msgs::msg::PoseStamped>("/smap/sampler/pose", 10);
+    pcl_pub = this->create_publisher<sensor_msgs::msg::PointCloud2>("/smap/sampler/pcl", 10);
+
+    // Subscriptions
+    image_sub = this->create_subscription<sensor_msgs::msg::Image>(
+      "/zed2/zed_node/rgb/image_rect_color",
+      10,
+      std::bind(&smap::smap_sampler_node::image_callback, this, _1),
+      this->sub_options
+    );
+    pcl2_sub = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+      "/zed2/zed_node/point_cloud/cloud_registered",
+      10,
+      std::bind(&smap::smap_sampler_node::pcl2_callback, this, _1),
+      this->sub_options
+    );
+
   }
-  ~smap_sampler()
+  ~smap_sampler_node()
   {
+    // Semaphores
+    sem_destroy(&this->sem_image);
+    sem_destroy(&this->sem_pcl);
   }
 
   void on_process(void) // Pooling
@@ -99,8 +128,6 @@ private:
 
   bool get_transform(const std::string &target_frame, const std::string &source_frame, geometry_msgs::msg::TransformStamped &transform)
   {
-    // https://docs.ros.org/en/foxy/Tutorials/Intermediate/Tf2/Writing-A-Tf2-Listener-Cpp.html
-    //tf_buffer->lookupTransform()
     try {
       transform = tf_buffer->lookupTransform(
         target_frame, source_frame,
@@ -113,7 +140,6 @@ private:
         source_frame.c_str(), target_frame.c_str(), ex.what()
       );
       return true;
-      //tf_buffer->waitForTransform()
     }
     return false;
   }
@@ -142,9 +168,9 @@ private:
   { 
 
     static bool invalid = false;
+
     // Sample Position
     static smap_interfaces::msg::SmapData msg;
-
     static geometry_msgs::msg::PoseStamped current_pose;
     static geometry_msgs::msg::TransformStamped transform;
 
@@ -162,8 +188,11 @@ private:
       msg.stamped_pose.pose.position.y = transform.transform.translation.y;
       msg.stamped_pose.pose.position.z = transform.transform.translation.z;
       msg.stamped_pose.pose.orientation = transform.transform.rotation;
+
+      // Set image and pcl semaphores
+      sem_wait(&(this->sem_image));
+      sem_wait(&(this->sem_pcl));
     }
-    // TODO: Implement semaphores
 
     // Sample Image
     if(!invalid){
@@ -187,6 +216,7 @@ private:
         msg.rgb_image.encoding = this->last_image_msg->encoding;
         msg.rgb_image.data = this->last_image_msg->data;
       }
+      sem_post(&(this->sem_image));
     }
 
     // Sample Point Cloud 2
@@ -209,6 +239,7 @@ private:
         tf2::doTransform(*(this->last_pcl2_msg),msg.pointcloud,transform);
         this->pcl_pub->publish(msg.pointcloud);
       }
+      sem_post(&(this->sem_pcl));
     }
 
     // Publish msg
@@ -223,14 +254,24 @@ private:
 
   void image_callback(sensor_msgs::msg::Image::SharedPtr msg)
   {
-    this->last_image_msg = msg;
+    if(sem_trywait(&(this->sem_image))){ // True -> semaphore not acquired!
+      return;
+    }else{
+      this->last_image_msg = msg;
+      sem_post(&(this->sem_image));
+    }
   }
 
   void pcl2_callback(sensor_msgs::msg::PointCloud2::SharedPtr msg)
   {
-    this->last_pcl2_msg = msg;
+    if(sem_trywait(&(this->sem_pcl))){ // True -> semaphore not acquired!
+      return;
+    }else{
+      this->last_pcl2_msg = msg;
+      sem_post(&(this->sem_pcl));
+    }
   }
-//public:
+
   
 };
 
@@ -243,20 +284,22 @@ int main(int argc, char ** argv)
 
   rclcpp::init(argc,argv);
 
-  //while(rclcpp::ok()){
-  //  try{
-  //      _smap_node->on_process(); // Pooling
-  //      _topological_map_node->on_process(); // Pooling
-  //      executor.spin_once();
-  //    }catch (std::exception& e){
-  //      std::cout << "Exception!" << std::endl;
-  //      std::cout << e.what() << std::endl;
-  //    }
-  //  }
-  //  rclcpp::shutdown();
-  //}
+  // Launch Node
+  std::shared_ptr<smap::smap_sampler_node> _smap_sampler_node = std::make_shared<smap::smap_sampler_node>();
 
-  rclcpp::spin(std::make_shared<smap::smap_sampler>());
+  // Add nodes to executor
+  rclcpp::executors::MultiThreadedExecutor executor;
+  executor.add_node(_smap_sampler_node);
+
+  while (rclcpp::ok()) {
+    try{
+      _smap_sampler_node->on_process(); // Pooling
+      executor.spin_once();
+    }catch (std::exception& e){
+      std::cout << "Exception!" << std::endl;
+      std::cout << e.what() << std::endl;
+    }
+  }
 
   rclcpp::shutdown();
 
